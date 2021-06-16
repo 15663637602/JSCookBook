61406f15b9e1c5f2266961d7a9f1bb13e777c9be
yuqi.li <243583762@qq.com>
2021-06-16 23:53:55 +0800

    day1 function

---
+++ DebugUtils.js
@@ -0,0 +1,15 @@
+function p(msg) {
+    document.write(msg + "\<br\>");
+}
+
+function sep() {
+    document.write('<hr color = "red"/>');
+}
+
+function enhance(msg) {
+    document.write('<strong>' + msg + '</strong>' + '<br>');
+}
+
+function b(msg) {
+    document.write('<b>' + msg + '</b>' + '<br>');
+}
